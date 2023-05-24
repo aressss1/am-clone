04280626aae87ae1e128b800a20ed3d3e89b3721
aressss1 <codewithfaizi@gmail.com>
2023-05-24 16:26:55 +0500

    101%

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -32,21 +32,3 @@ const Banner = () => {
 };
 
 export default Banner;
-
-// import { Carousel } from "react-responsive-carousel"
-
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
-
-// const settings = {
-//     infinite: true,
-//     speed: 500,
-//     slidesToShow: 1,
-//     slidesToScroll: 1,
-//     autoplay: true,
-//     autoplaySpeed: 5000,
-//     // cssEase: "linear"
-// };
-
-// import Slider from "react-slick";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
